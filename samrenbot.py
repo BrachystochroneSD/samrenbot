5e7d23a05d2d80ff9539ee52b1f98d9caf549bb4
Samuel Dawant <samrenfou@hotmail.com>
2020-08-08 22:41:17 +0200

    rename in samrenbot.py

--- samrenbot.py
+++ samrenbot.py
@@ -1,9 +1,9 @@
 import configparser
 
 import irc_class as ic
-# import twitch_alerts.webhooks_subs as taws
+import minylpg.minylpg as miny
+from twitch_alerts.webhooks_subs import Follow_Subscription
 
-# import minitel_announcement as ma #TODO
 
 config = configparser.ConfigParser()
 config.read('config.ini')
@@ -16,16 +16,39 @@ BOTPASS = config['IRC']['BOTPASS']
 CHANNEL = config['IRC']['CHANNEL']
 SERVER = config['IRC']['SERVER']
 
-irc=ic.IRC()
+# IRC
+irc = ic.IRC()
 irc.connect(SERVER, PORT, CHANNEL, BOTNICK, BOTPASS)
-follower_file = open('twitch_alerts/follow_alert.txt', 'w+')
+
+# Subscription to twitch follow alert
+follow_subs = Follow_Subscription(channel_id, client_id, lease_seconds, callback_url, oauth_token)
+
+# Minitel
+minylpg = miny.MinYLPG()
 
 while True:
     text = irc.get_response()
     user = text.split(":")[1].split("!")[0].capitalize()
     message = text.split(":")[-1]
 
+    with open('twitch_alerts/follow_alert.txt', 'r+') as follower_file:
+        file_lines = follower_file.readlines()
+
+        if len(file_lines) != 0:
+            name = file_lines[0]
+            print("New follower:", name)
+            if name != "":
+                minylpg.follower_alert(file_lines[0])
+
+            # Delete firt line
+            next_lines = follower_file.readlines()[1:]
+            follower_file.seek(0)
+            for i in next_lines:
+                follower_file.write(i)
+            follower_file.truncate()
+
     if "PRIVMSG" in text:
         print(user,":",message)
+        minylpg.message(user + ":" + message)
     if "!hello" in message:
         irc.send(CHANNEL, 'Hello {}!'.format(user))
